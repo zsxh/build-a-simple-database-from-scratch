517178cd5c9377a7f262fa03765f1fd978f6d846
zsxh <bnbvbchen@gmail.com>
2020-02-28 15:40:22 +0800

    Part 2 - World's Simplest SQL Compiler and Virtual Machine
    C doesn't support exceptions so we're using enum result codes
    
    now sql compiler can only recognize .exit meta-command, and sql
    keywords "insert" and "select"
    
    virtual machine only print a message if its "insert"/"select" sql

--- db.c
+++ db.c
@@ -13,6 +13,19 @@ typedef struct {
   ssize_t input_length;
 } InputBuffer;
 
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
+
 InputBuffer *new_input_buffer() {
   InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
@@ -44,17 +57,67 @@ void close_input_buffer(InputBuffer *input_buffer) {
   free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
+  if (!strcmp(input_buffer->buffer, ".exit")) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer,
+                                Statement *statement) {
+  if (!strncmp(input_buffer->buffer, "insert", 6)) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+
+  if (!strncmp(input_buffer->buffer, "select", 6)) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
+}
+
 int main() {
   InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
   while (true) {
     print_prompt();
     read_input(input_buffer);
 
-    if (!strcmp(input_buffer->buffer, ".exit")) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    if (input_buffer->buffer[0] == '.') {
+      // Non-SQL statements like .exit are called "meta-commands".
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized comand '%s'\n", input_buffer->buffer);
+          continue;
+      }
     }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+        continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
