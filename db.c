90b192ad47d49653a29dee9025574c5f8c51b7f6
zsxh <bnbvbchen@gmail.com>
2020-03-03 18:23:08 +0800

    Part 4 - Our First Tests (and Bugs)
    - Using rspec to write tests
    - fix string end with a null character/overflow bugs
    - replace sscanf() with strtok()

--- db.c
+++ db.c
@@ -23,6 +23,8 @@ typedef enum {
 
 typedef enum {
   PREPARE_SUCCESS,
+  PREPARE_NEGATIVE_ID,
+  PREPARE_STRING_TOO_LONG,
   PREPARE_SYNTAX_ERROR,
   PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
@@ -33,8 +35,8 @@ typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 #define COLUMN_EMAIL_SIZE 255
 typedef struct {
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct {
@@ -107,7 +109,7 @@ void free_table(Table *table) {
 }
 
 InputBuffer *new_input_buffer() {
-  InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+  InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
   input_buffer->buffer_length = 0;
   input_buffer->input_length = 0;
@@ -147,17 +149,40 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table) {
   }
 }
 
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement) {
+  statement->type = STATEMENT_INSERT;
+
+  char *keyword = strtok(input_buffer->buffer, " ");
+  char *id_string = strtok(NULL, " ");
+  char *username = strtok(NULL, " ");
+  char *email = strtok(NULL, " ");
+
+  if (id_string == NULL || username == NULL || email == NULL) {
+    return PREPARE_SYNTAX_ERROR;
+  }
+
+  int id = atoi(id_string);
+  if (id < 0) {
+    return PREPARE_NEGATIVE_ID;
+  }
+  if (strlen(username) > COLUMN_USERNAME_SIZE) {
+    return PREPARE_STRING_TOO_LONG;
+  }
+  if (strlen(email) > COLUMN_EMAIL_SIZE) {
+    return PREPARE_STRING_TOO_LONG;
+  }
+
+  statement->row_to_insert.id = id;
+  stpcpy(statement->row_to_insert.username, username);
+  strcpy(statement->row_to_insert.email, email);
+
+  return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer *input_buffer,
                                 Statement *statement) {
   if (!strncmp(input_buffer->buffer, "insert", 6)) {
-    statement->type = STATEMENT_INSERT;
-    int args_assigned = sscanf(
-        input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-        statement->row_to_insert.username, statement->row_to_insert.email);
-    if (args_assigned < 3) {
-      return PREPARE_SYNTAX_ERROR;
-    }
-    return PREPARE_SUCCESS;
+    return prepare_insert(input_buffer, statement);
   }
 
   if (!strncmp(input_buffer->buffer, "select", 6)) {
@@ -201,7 +226,7 @@ ExecuteResult execute_statement(Statement *statement, Table *table) {
 
 int main() {
   Table *table = new_table();
-  InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+  InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
   while (true) {
     print_prompt();
     read_input(input_buffer);
@@ -221,6 +246,12 @@ int main() {
     switch (prepare_statement(input_buffer, &statement)) {
       case (PREPARE_SUCCESS):
         break;
+      case (PREPARE_NEGATIVE_ID):
+        printf("ID must be positive.\n");
+        continue;;
+      case (PREPARE_STRING_TOO_LONG):
+        printf("String is too long.\n");
+        continue;
       case (PREPARE_SYNTAX_ERROR):
         printf("Syntax error. Could not parse statement.\n");
         continue;
